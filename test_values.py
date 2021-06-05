bb121ad48ae49cd3c2020d0d3f7c22b78427e01a
Juan Arancibia <arancibia.juan22@gmail.com>
2021-06-05 13:38:57 -0300

    Added test tweets

--- test_values.py
+++ test_values.py
@@ -2,4 +2,6 @@ test_values = [(0, 'Negativo'), (1, 'Negativo'), (7, 'Negativo'), (12, 'Negativo
 (39, 'Negativo') ,(57, 'Negativo'), (62, 'Negativo'), (82, 'Negativo'), (91, 'Negativo'), (97, 'Negativo'), (452, 'Negativo'), (2198, 'Negativo'), 
 (44, 'Positivo') ,(2146, 'Positivo') ,(486, 'Positivo'), (1008, 'Positivo'), (1018, 'Positivo'), (1030, 'Positivo'), (1063, 'Positivo'), (52434, 'Positivo'), 
 (52198, 'Positivo'), (52068, 'Positivo'), (2, 'Neutro') ,(3, 'Neutro'), (8, 'Neutro'), (29, 'Neutro'), (70, 'Neutro') ,(76, 'Neutro'), (79, 'Neutro'), 
-(94, 'Neutro'), (690, 'Neutro'), (1041, 'Neutro'), (9650, 'Neutro'), (82213, 'Neutro'), (54455, 'Neutro'), (54360, 'Neutro'), (54313, 'Neutro'), (53418, 'Neutro')]
+(94, 'Neutro'), (690, 'Neutro'), (1041, 'Neutro'), (9650, 'Neutro'), (82213, 'Neutro'), (54455, 'Neutro'), (54360, 'Neutro'), (54313, 'Neutro'), (53418, 'Neutro'),
+(19, 'Negativo'), (205207, 'Negativo'), (205358, 'Negativo'), (205363, 'Neutro'), (205372, 'Negativo'), (205390, 'Neutro'), (205397, 'Negativo'),
+ (205405, 'Negativo'),(205409, 'Negativo')]
