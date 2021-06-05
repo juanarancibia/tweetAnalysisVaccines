f6a92492410cb3343b39f70df661999dd65862e4
Juan Arancibia <arancibia.juan22@gmail.com>
2021-06-04 21:05:00 -0300

    Extracted test values to separate file

--- test_sentiment_analysis.py
+++ test_sentiment_analysis.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import sentimentAnalysis
 import tweetFile
+import test_values
 
 df = pd.read_csv('./Datos/vacunasXFraseLimpiasConStopwords.csv')
 
 df['index'] = df['Unnamed: 0']
 df.drop(df.columns.difference(['index','tweet']),1,inplace=True)
 
-test_values = [(0, 'Negativo'), (1, 'Negativo'), (7, 'Negativo'), (12, 'Negativo'), (14, 'Negativo'), (18, 'Negativo'), (27, 'Negativo'), (39, 'Negativo') ,(57, 'Negativo'), (62, 'Negativo'), (82, 'Negativo'), (91, 'Negativo'), (97, 'Negativo'), (452, 'Negativo'), (2198, 'Negativo'), 
-(44, 'Positivo') ,(2146, 'Positivo') ,(486, 'Positivo'), (1008, 'Positivo'), (1018, 'Positivo'), (1030, 'Positivo'), (1063, 'Positivo'), (52434, 'Positivo'), (52198, 'Positivo'), (52068, 'Positivo'), 
-(2, 'Neutro') ,(3, 'Neutro'), (8, 'Neutro'), (29, 'Neutro'), (70, 'Neutro') ,(76, 'Neutro'), (79, 'Neutro'), (94, 'Neutro'), (690, 'Neutro'), (1041, 'Neutro'), (9650, 'Neutro'), (82213, 'Neutro'), (54455, 'Neutro'), (54360, 'Neutro'), (54313, 'Neutro'), (53418, 'Neutro')]
-
-sorted_by_index = sorted(test_values, key=lambda x: x[0])
+sorted_by_index = sorted(test_values.test_values, key=lambda x: x[0])
 list_indexes, list_test_values = zip(*sorted_by_index)
 df = df[(df['index'].isin(list_indexes))]
-df['sentiment_test'] = list_test_values
 
+df['sentiment_test'] = list_test_values
 df['sentiment_value'] = df['tweet'].map(lambda x: sentimentAnalysis.analize_sentiment_value(str(x)))
 df['sentiment_1st'] = df['tweet'].map(lambda x: sentimentAnalysis.analize_sentiment_discretized_1st(str(x)))
 df['sentiment_2nd'] = df['tweet'].map(lambda x: sentimentAnalysis.analize_sentiment_discretized_2nd(str(x)))
---
+++ test_values.py
@@ -0,0 +1,5 @@
+test_values = [(0, 'Negativo'), (1, 'Negativo'), (7, 'Negativo'), (12, 'Negativo'), (14, 'Negativo'), (18, 'Negativo'), (27, 'Negativo'), 
+(39, 'Negativo') ,(57, 'Negativo'), (62, 'Negativo'), (82, 'Negativo'), (91, 'Negativo'), (97, 'Negativo'), (452, 'Negativo'), (2198, 'Negativo'), 
+(44, 'Positivo') ,(2146, 'Positivo') ,(486, 'Positivo'), (1008, 'Positivo'), (1018, 'Positivo'), (1030, 'Positivo'), (1063, 'Positivo'), (52434, 'Positivo'), 
+(52198, 'Positivo'), (52068, 'Positivo'), (2, 'Neutro') ,(3, 'Neutro'), (8, 'Neutro'), (29, 'Neutro'), (70, 'Neutro') ,(76, 'Neutro'), (79, 'Neutro'), 
+(94, 'Neutro'), (690, 'Neutro'), (1041, 'Neutro'), (9650, 'Neutro'), (82213, 'Neutro'), (54455, 'Neutro'), (54360, 'Neutro'), (54313, 'Neutro'), (53418, 'Neutro')]
